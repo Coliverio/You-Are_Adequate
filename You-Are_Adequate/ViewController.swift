9fca39e835cbac3be425071a766bae6cc1555390
Claire Oliverio <coliverio@zagmail.gonzaga>
2022-01-22 17:13:07 -0800

    Initial Commit

---
+++ You-Are_Adequate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You-Are_Adequate
+//
+//  Created by Claire Oliverio  on 1/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
