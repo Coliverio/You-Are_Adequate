9a8422b2c17a4fb0961b9eb3bb7e4b6d4ed4f317
Claire Oliverio <coliverio@zagmail.gonzaga>
2022-01-24 17:31:29 -0800

    finished 2.6 with Gallagher

--- You-Are_Adequate/ViewController.swift
+++ You-Are_Adequate/ViewController.swift
@@ -9,16 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("👍🏻 viewDidLoad has run!")
-        messageLabel.text = "Fabulous? That's you!!"
+    
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        print("😎 The message button was pressed!")
         messageLabel.text = "You are Adequate!"
+        imageView.image = UIImage (named: "image8")
     }
     
 }
